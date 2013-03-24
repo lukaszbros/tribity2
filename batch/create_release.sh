83e11ef0f38388e8aed4feb84a487eba78c87449
lukaszbros <lukasz.bros@gmail.com>
2013-03-24 20:43:50 +0100

    Updated release script to add *.php files

--- batch/create_release.sh
+++ batch/create_release.sh
@@ -10,7 +10,9 @@ mkdir release/
 cp --recursive css release/css
 cp --recursive img release/img
 cp --recursive js release/js
+cp --recursive js release/php
 cp *.html release
 cp *.png release
 cp *.html release
 cp *.ico release
+cp *.php release
