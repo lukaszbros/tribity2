9b5523bd5e0c3e45000eaed924a19bbe74f6d391
lukaszbros <lukasz.bros@gmail.com>
2013-03-25 02:02:53 +0100

    Release for production

--- batch/create_release.sh
+++ batch/create_release.sh
@@ -10,7 +10,7 @@ mkdir release/
 cp --recursive css release/css
 cp --recursive img release/img
 cp --recursive js release/js
-cp --recursive js release/php
+cp --recursive php release/php
 cp *.html release
 cp *.png release
 cp *.html release
